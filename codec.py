c22c866afa634e936ff4aa4ff8c3a08ecf0ab294
Alejandro Valdez <alejandrovaldez587@gmail.com>
2021-05-26 09:22:02 -0300

    prueba2

---
+++ codec.py
@@ -0,0 +1,7 @@
+while True:
+    mensaje = []
+    mensaje = (input("ingrese mensaje a codificar: "))
+    palabrare = ""
+    for i in mensaje:
+        palabrare = i + palabrare
+    print (palabrare)
