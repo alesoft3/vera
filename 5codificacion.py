9664cc5fa8f615134ab0a1fc589838553b768e15
Alejandro Valdez <alejandrovaldez587@gmail.com>
2021-05-26 02:52:30 -0300

    agregando mis codigos

---
+++ 5codificacion.py
@@ -0,0 +1,9 @@
+while True:
+    mensaje = []
+    mensaje = (input("ingrese mensaje a codificar: "))
+    palabrare = ""
+    for i in mensaje:
+        palabrare = i + palabrare
+    print (palabrare)
+
+
