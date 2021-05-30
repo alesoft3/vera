7c3989446797eb582fd92c09eef89001b2d1e961
Alejandro Valdez <alejandrovaldez587@gmail.com>
2021-05-29 23:58:43 -0300

    agregado

---
+++ snake2.py
@@ -0,0 +1,109 @@
+import curses
+import random
+import time
+# from playsound import playsound
+from curses import textpad
+import threading
+
+#menu= ["nuevo juego" "salir"]
+#hi_score = 0
+# prueba en git
+alto=25
+ancho=100
+pantalla = curses.initscr()
+
+#puntuación = 0
+#score_text = "Puntuación: {}", format (puntuación)
+#hi_score_text = "Puntuación alta: {}", format (hi_score)
+#stdscr.addtr(1, sw//2-len(score_text))//2,score_text)
+#stdscr.agregatr (1,4,hi_score_text)
+#============================
+
+#===inicializacion de curses=====
+#============================
+
+curses.start_color() 
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_YELLOW)
+
+curses.noecho()
+curses.cbreak()
+curses.curs_set(0)
+pantalla.keypad(True)
+pantalla.nodelay(True)
+
+#score =+10
+    
+#============================
+#===inicializacion de snake=====
+#===y de la fruta=====
+#============================
+
+x=0
+y=0
+snake=[[y,x]]
+fruta=[random.randrange(alto),random.randrange(ancho)]
+pantalla.addstr(fruta[0],fruta[1],"#")
+mov_horizontal = 1
+mov_vertical = 0
+
+#============================
+#===comienzo del juego=====
+#============================
+#def pantalla (stdscr):
+ #   curses.curs_set(0)
+  #  stdscr.nodelay(1)
+  #  stdscr.timeout(100)
+while True:    
+    #leemos la tecla
+    tecla = pantalla.getch()        
+    #le asignamos una dirección a cada cursor
+    if tecla==curses.KEY_LEFT:
+        mov_horizontal = -1
+        mov_vertical = 0
+    elif tecla==curses.KEY_RIGHT:
+        mov_horizontal = 1
+        mov_vertical = 0
+    elif tecla==curses.KEY_UP:
+        mov_horizontal = 0
+        mov_vertical = -1
+    elif tecla==curses.KEY_DOWN:
+        mov_horizontal = 0
+        mov_vertical = 1
+    elif tecla==10:
+        break
+    
+    x = (x + mov_horizontal) % ancho
+    y = (y + mov_vertical) % alto
+
+    posicion=[y,x]
+
+    #si nos comemos una parte de snake, el juego se enoja y se sale    
+    if posicion in snake:
+        break
+
+    snake.insert(0,posicion)    
+    #ponemos una pausa, probá que sucede si la comentás
+    time.sleep(0.1)
+
+    #verificamos si nos morfamos una fruta
+    if fruta in snake:        
+        fruta=[random.randrange(alto),random.randrange(ancho)]
+        pantalla.addstr(fruta[0],fruta[1],"#")
+    
+    #sino, borramos la colita
+    else:
+        ultimaposicion = snake.pop()
+        pantalla.addstr(ultimaposicion[0],ultimaposicion[1]," ")
+    #else  
+    #   ultimaposicion = snake.pop()
+    #   pantalla.addstr(ultimaposicion[0],ultimaposicion[1]," ")
+    #dibujamos la cabeza
+        pantalla.addstr(snake[0][0],snake[0][1],"#")
+
+    #pantalla.addstr(y,x, "#")   
+    
+#Ponemos todo en su lugar    
+curses.nocbreak()
+pantalla.keypad(False)
+curses.echo()
+curses.endwin()
