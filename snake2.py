69c84510bd193c55e442886b988f7c5b8eca3a40
Alejandro Valdez <alejandrovaldez587@gmail.com>
2021-06-03 17:21:10 -0300

    agregado fruta

--- snake2.py
+++ snake2.py
@@ -4,26 +4,29 @@ import time
 # from playsound import playsound
 from curses import textpad
 import threading
+#menu
+menu= ["nuevo juego" "salir"]
+hi_score = 0
 
-#menu= ["nuevo juego" "salir"]
-#hi_score = 0
 # prueba en git
 alto=25
 ancho=100
 pantalla = curses.initscr()
 
-#puntuación = 0
-#score_text = "Puntuación: {}", format (puntuación)
-#hi_score_text = "Puntuación alta: {}", format (hi_score)
+puntuación = 0
+score_text = "Puntuación: {}", format (puntuación)
+hi_score_text = "Puntuación alta: {}", format (hi_score)
 #stdscr.addtr(1, sw//2-len(score_text))//2,score_text)
-#stdscr.agregatr (1,4,hi_score_text)
-#============================
-
-#===inicializacion de curses=====
-#============================
-
+#stdscr.agregar (1,4,hi_score_text)
+#=================
+#Color de pantalla
+#==================
 curses.start_color() 
 curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_YELLOW)
+
+#============================
+#===inicializacion de curses=====
+#============================
 
 curses.noecho()
 curses.cbreak()
@@ -31,13 +34,13 @@ curses.curs_set(0)
 pantalla.keypad(True)
 pantalla.nodelay(True)
 
+
 #score =+10
     
 #============================
 #===inicializacion de snake=====
 #===y de la fruta=====
 #============================
-
 x=0
 y=0
 snake=[[y,x]]
@@ -45,14 +48,22 @@ fruta=[random.randrange(alto),random.randrange(ancho)]
 pantalla.addstr(fruta[0],fruta[1],"#")
 mov_horizontal = 1
 mov_vertical = 0
+#otra fruta
+fruta=[random.randrange(alto),random.randrange(ancho)]
+pantalla.addstr(fruta[0],fruta[1],"ñ")
+mov_horizontal= 1
+mov_vertical = 0
+# pantalla
 
+#def pantalla (stdscr):
+#    curses.curs_set(0)
+#    stdscr.nodelay(1)
+#    stdscr.timeout(100)
+##    current_row = 0
+ #   print (menu(stdscr,current_row))
 #============================
 #===comienzo del juego=====
 #============================
-#def pantalla (stdscr):
- #   curses.curs_set(0)
-  #  stdscr.nodelay(1)
-  #  stdscr.timeout(100)
 while True:    
     #leemos la tecla
     tecla = pantalla.getch()        
@@ -89,19 +100,22 @@ while True:
     if fruta in snake:        
         fruta=[random.randrange(alto),random.randrange(ancho)]
         pantalla.addstr(fruta[0],fruta[1],"#")
-    
+    elif fruta in snake:
+        fruta=[random.randrange(alto),random.randrange(ancho)]
+        pantalla.addstr(fruta[0], fruta[1],"ñ")
     #sino, borramos la colita
     else:
         ultimaposicion = snake.pop()
         pantalla.addstr(ultimaposicion[0],ultimaposicion[1]," ")
-    #else  
-    #   ultimaposicion = snake.pop()
-    #   pantalla.addstr(ultimaposicion[0],ultimaposicion[1]," ")
+
+    #else:  
+    #    ultimaposicion = snake.pop()
+    #    pantalla.addstr(ultimaposicion[0],ultimaposicion[1]," ")
     #dibujamos la cabeza
         pantalla.addstr(snake[0][0],snake[0][1],"#")
+        pantalla.addstr(snake[0][0], snake[0][1],"ñ")
 
     #pantalla.addstr(y,x, "#")   
-    
 #Ponemos todo en su lugar    
 curses.nocbreak()
 pantalla.keypad(False)
